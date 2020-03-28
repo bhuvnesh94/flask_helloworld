c20aade704543ee85ac67342ceea04f4648a16b1
bhuvnesh sharma <bhuvnesh1510@gmail.com>
2020-03-28 16:50:00 +0000

    edited again

--- hello.py
+++ hello.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
-    return "Edited first time"
+    return "Hello World! Edited first time"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
